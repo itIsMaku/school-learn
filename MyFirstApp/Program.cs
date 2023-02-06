b3f17e5a5b945f3139e04b01e29f52338d7d387a
Volkman Adam - IT2 <volkmanadam215@student.voskh.cz>
2023-02-06 15:22:37 +0100

    idk lol

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -8,8 +8,22 @@ namespace MyFirstApp
 {
     class Program
     {
+        private static List<string> texts = new List<string>();
+
         static void Main(string[] args)
         {
+            string text = "ja opravdu nevim co mam o te hodine delat, protoze se tu nudim jak kkt";
+            string[] splittedText = text.Split(' ');
+            texts.AddRange(splittedText);
+            Console.Title = "cuspic";
+            Console.BackgroundColor = ConsoleColor.Red;
+            //Console.SetCursorPosition(Console.WindowWidth / 2, 1);
+            for (int i = 0; i < texts.Count; i++)
+            {
+                Console.WriteLine(texts.ElementAt(i));
+            }
+            Console.ReadKey();
+            
         }
     }
 }
