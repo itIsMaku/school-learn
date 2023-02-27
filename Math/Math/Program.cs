7f414fe4c6d56339ac315f741089a704511632ab
Volkman Adam - IT2 <volkmanadam215@student.voskh.cz>
2023-02-27 15:22:28 +0100

    idk cs

---
+++ Math/Math/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // x^2 + 20x + 3 = 0
+            int a = 1;
+            int b = 20;
+            int c = 3;
+            double x1 = System.Math.Round((-b + System.Math.Sqrt(System.Math.Pow(b, 2) - 4 * a * c)) / 2 * a);
+            double x2 = System.Math.Round((-b - System.Math.Sqrt(System.Math.Pow(b, 2) - 4 * a * c)) / 2 * a);
+            Console.WriteLine("x1 = " + x1);
+            Console.WriteLine("x2 = " + x2);
+
+            double r;
+            double v;
+            try
+            {
+                Console.WriteLine("Zadej průměr v cm.");
+                r = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Zadej výšku v cm.");
+                v = Double.Parse(Console.ReadLine());
+            } catch
+            {
+                Console.WriteLine("Zadal jsi špatný typ.");
+                Console.ReadKey();
+                return;
+            }
+            // π r²
+            double result = System.Math.PI * System.Math.Pow(r, 2) * v;
+            Console.WriteLine("Objem sudu v litrech: " + result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ volkman-operatory2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace volkman_operatory2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            string uvitani = "Vítejte";
+            bool srovnani = a > b;
+            bool rovnost = a == b;
+            int soucet = a + b;
+            int soucin = a * b;
+            int rozdil = a - b;
+            double x = a;
+            double y = b;
+            double podil = x / y;
+
+            Console.WriteLine(uvitani);
+            Console.WriteLine("srovnani " + srovnani);
+            Console.WriteLine("negace rovnosti " + !rovnost);
+            Console.WriteLine("soucet " + soucet);
+            Console.WriteLine("soucin " + soucin);
+            Console.WriteLine("rozdil " + rozdil);
+            Console.WriteLine("podil " + podil);
+            Console.ReadKey();
+        }
+    }
+}
